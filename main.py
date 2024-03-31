083acbe6078a6b795a166907e67d4e529bf7f9a6
ethonwilles <ethonwilles@gmail.com>
2024-03-30 22:02:56 -0600

    fixed badly named variables

--- main.py
+++ main.py
@@ -122,11 +122,11 @@ while True:
             
             frequency_data = {}
             with open("/run/dump1090-fa/frequency.json", "r") as freq_file:
-                frequency_data = json.load(freq_file)
+                frequency_data = json.load(freq_file.read())
                 freq_file.close()
             with open("/run/dump1090-fa/frequency.json", "w") as freq_file:
                 updated_frequency_data = frequency_data[datetime.datetime.today().strftime("%m/%d/%y")] = daily_hex
-                file.write(json.dumps(updated_frequency_data))
+                freq_file.write(json.dumps(updated_frequency_data))
                 freq_file.close()
             daily_hex = {}
             sent_daily = True
