31497ab211b6c3d41536c9a58911fa121ff65981
ethonwilles <ethonwilles@gmail.com>
2024-03-30 21:20:52 -0600

    removed keys

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
 meshtastic_channel_index = 0
-discord_webhook = "https://discord.com/api/webhooks/1187457672702861382/pPVl83gXyT7erNBjg-Lc8RA9ECDLqSEMSjzD-2pSFOOKzeJzh63ilXu3dBUVItZOBsTu"
+discord_webhook = ""
 discord_error_webhook = ""
